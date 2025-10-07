0aec8be9b5081a015f9a0a7c133042f37947fa29
robotai <robotai@V-2204-HPLC-09.staff.ad.curtin.edu.au>
2025-10-07 10:55:08 +0800

    tut7 up to 4.0

---
+++ src/iar_dijkstra_planner/include/iar_dijkstra_planner/iar_dijkstra_planner.hpp
@@ -0,0 +1,19 @@
+#ifndef IAR_DIJKSTRA_PLANNER__IAR_DIJKSTRA_PLANNER_HPP_
+#define IAR_DIJKSTRA_PLANNER__IAR_DIJKSTRA_PLANNER_HPP_
+
+#include "iar_dijkstra_planner/visibility_control.h"
+
+namespace iar_dijkstra_planner
+{
+
+class IarDijkstraPlanner
+{
+public:
+  IarDijkstraPlanner();
+
+  virtual ~IarDijkstraPlanner();
+};
+
+}  // namespace iar_dijkstra_planner
+
+#endif  // IAR_DIJKSTRA_PLANNER__IAR_DIJKSTRA_PLANNER_HPP_
---
+++ src/iar_dijkstra_planner/src/iar_dijkstra_planner.cpp
@@ -0,0 +1,14 @@
+#include "iar_dijkstra_planner/iar_dijkstra_planner.hpp"
+
+namespace iar_dijkstra_planner
+{
+
+IarDijkstraPlanner::IarDijkstraPlanner()
+{
+}
+
+IarDijkstraPlanner::~IarDijkstraPlanner()
+{
+}
+
+}  // namespace iar_dijkstra_planner
